c92733f593c55a49529c7b9e200fc8746f38530d
Farai Rubvuta <farairubvuta@gmail.com>
2026-01-17 04:22:41 +0200

    validation functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let heroMobile = document.querySelector(".hero-mobile");
 let heroDesktop = document.querySelector(".hero-desktop");
 let container = document.querySelector(".container")
+let submitButton = document.querySelector("form button")
+let emailInput = document.querySelector("#email")
 
-const mediaQuery = window.matchMedia("(max-width: 1024px)");
+
+const isDesktop = window.matchMedia("(min-width: 1024px)");
 
 function show (element) {
     if (element.classList?.contains("hidden")) {
@@ -15,9 +18,17 @@ function hide (element) {
         element.classList?.add("hidden")
     }
 }
+
 // Define a callback function to run when the state changes
 function handleMediaChange (event) {
     if (event.matches) {
+        // Desktop screen logic
+        console.log("Desktop View");
+        show(heroDesktop)
+        hide(heroMobile)
+        container.classList?.add("desktop")
+
+    } else {
         // Mobile screen logic
         console.log("Mobile View");
         show(heroMobile)
@@ -25,18 +36,37 @@ function handleMediaChange (event) {
         if (container.classList?.contains("desktop")) {
             container.classList?.remove("desktop")
         }
-
-    } else {
-        // Desktop screen logic
-        console.log("Desktop View");
-        show(heroDesktop)
-        hide(heroMobile)
-        container.classList?.add("desktop")
     }
 }
 
-// Add the event listener (modern approach)
-mediaQuery.addEventListener("change", handleMediaChange);
+// Add the event listener
+isDesktop.addEventListener("change", handleMediaChange);
+
+// For Submission logic
+submitButton.addEventListener("click", handleSubmit)
+// form.addEventListener("submit", handleSubmit)
 
 // Initial check when the script loads
-handleMediaChange(mediaQuery);
+handleMediaChange(isDesktop);
+
+// Form submission logic
+function handleSubmit (event) {
+    event.preventDefault()
+    let email = emailInput.value.trim()
+
+    if (email == "" || emailInput.matches(":invalid")) {
+        invalidateForm()
+    }
+    else {
+        validateForm()
+    }
+}
+
+function invalidateForm () {
+    if (emailInput.classList?.contains("invalid")) return
+    emailInput.classList.add("invalid")
+}
+function validateForm () {
+    if (!emailInput.classList?.contains("invalid")) return
+    emailInput.classList.remove("invalid")
+}
